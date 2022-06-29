4405f91ab3709a4048f38c57cc4793ad44710df7
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-29 18:26:16 -0500

    Añadidos middlewares a rutas /movies

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ const CharactersRoutes = require('./routes/characters.route');
 const GenresRoutes = require('./routes/genres.route');
 const MoviesRoutes = require('./routes/movies.route');
 const AuthRoutes = require('./routes/auth.route');
+const {checkToken, validateToken} = require('./middlewares/checkToken');
 
 router.use(CharactersRoutes);
 router.use(GenresRoutes);
-router.use(MoviesRoutes);
+router.use([checkToken, validateToken], MoviesRoutes);
 router.use(AuthRoutes);
 
 module.exports = router;
