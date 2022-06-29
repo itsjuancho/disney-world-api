52f53e0e5210841f937d42dee633e666c97c41c4
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-29 17:06:48 -0500

    Añadida ruta POST /movies y modificado controlador de GET /movies/:id

--- src/routes/movies.route.js
+++ src/routes/movies.route.js
@@ -1,7 +1,8 @@
 const route = require('express').Router();
 const MoviesController = require('../controllers/movies.controller');
 
+route.post('/movies', MoviesController.create);
 route.get('/movies', MoviesController.getAll);
-route.get('/movies/create', MoviesController.create);
+route.get('/movies/:id', MoviesController.getById);
 
 module.exports = route;
