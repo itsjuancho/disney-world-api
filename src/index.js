971f2d63234dcde68764b2a425dab1844ac62a6f
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-30 18:49:31 -0500

    Modificada ruta para obtener JSON de documentación Swagger

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 
 app.use("/api", routes);
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(require('../api.docs.json'), {
+app.use('/', swaggerUi.serve, swaggerUi.setup(require('../docs/api.docs.json'), {
     customCss: '.swagger-ui .topbar { display: none }',
     customSiteTitle: "Docs | Disney World API"
 }));
