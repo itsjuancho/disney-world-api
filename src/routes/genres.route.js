bb190d856c5ee4a541717789f98d4f835c4b09a0
Andrés Pérez <juanandresperezpt@gmail.com>
2022-06-30 22:57:29 -0500

    ARetiradas rutas no necesarias

--- src/routes/genres.route.js
+++ src/routes/genres.route.js
@@ -1,8 +1,6 @@
 const route = require('express').Router();
 const GenresController = require('../controllers/genres.controller');
 
-route.get('/getAll', GenresController.getAll);
-route.get('/:id/getMovies', GenresController.getMovies);
-route.get('/genres', GenresController.get);
+route.get('/', GenresController.getAll);
 
 module.exports = route;
